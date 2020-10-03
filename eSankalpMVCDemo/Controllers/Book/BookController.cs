03aadce6ec28c42e4ee75f47ac5d028ba35927c6
Esankalp Software <sachinmahore@gmail.com>
2020-10-03 17:02:29 +0530

    first upload by Sachin Mahore

---
+++ eSankalpMVCDemo/Controllers/Book/BookController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eSankalpMVCDemo.Models;
+
+namespace eSankalpMVCDemo.Controllers.Book
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            var model = new BookModel().GetBookList();
+            return View(model);
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(long id)
+        {
+            var model = new BookModel().GetBookDetails(id);
+            return View(model);
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        public ActionResult Create(BookModel bookModel)
+        {
+            try
+            {
+                var model = new BookModel().SaveBook(bookModel);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ eSankalpMVCDemo/Models/Book/BookModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using eSankalpMVCDemo.Data;
+
+namespace eSankalpMVCDemo.Models
+{
+    public class BookModel
+    {
+        public long BookID { get; set; }
+        public string BookName { get; set; }
+        public string ISBN { get; set; }
+        public string Author { get; set; }
+
+        public string SaveBook(BookModel model)
+        {
+            string msg = "";
+            eSankalpMVCDemoEntities db = new eSankalpMVCDemoEntities();
+            var saveBook = new tbl_Book()
+            {
+                BookName = model.BookName,
+                ISBN = model.ISBN,
+                Author = model.Author,
+            };
+            db.tbl_Book.Add(saveBook);
+            db.SaveChanges();
+            msg = "Book saved successfullly";
+            return msg;
+        }
+
+        public List<BookModel> GetBookList()
+        {
+            eSankalpMVCDemoEntities db = new eSankalpMVCDemoEntities();
+            List<BookModel> lstBook = new List<BookModel>();
+
+            var bookList = db.tbl_Book.ToList();
+            foreach(var book in bookList)
+            {
+                lstBook.Add(new BookModel
+                {
+                    BookID=book.BookID,
+                    BookName=book.BookName,
+                    ISBN=book.ISBN,
+                    Author=book.Author,
+                });
+            }
+            return lstBook;
+        }
+
+        public BookModel GetBookDetails(long BookID)
+        {
+            eSankalpMVCDemoEntities db = new eSankalpMVCDemoEntities();
+            BookModel bookModel = new BookModel();
+            var bookDet = db.tbl_Book.Where(p => p.BookID == BookID).FirstOrDefault();
+            
+            if(bookDet!=null)
+            {
+                bookModel.BookName = bookDet.BookName;
+                bookModel.ISBN = bookDet.ISBN;
+                bookModel.Author = bookDet.Author;
+                bookModel.BookID = bookDet.BookID;
+            }
+            return bookModel;
+        }
+    }
+}
